51d1cd1c379c360ca07bd6d841675a1203894b82
Yıldıray Koçak <kocakakay@gmail.com>
2024-03-08 09:01:17 +0300

    Update CONFIG.js

--- src/CONFIG.js
+++ src/CONFIG.js
@@ -1,6 +1,5 @@
 export const CONFIG = {
     // BaseUrl: "http://localhost:7590/api/", // PRODUCTION
-    // BaseUrl: "http://192.168.1.8:7590/api/", // PRODUCTION IP BASED
     BaseUrl: "https://localhost:7095/api/", // DEBUG
   };
   
